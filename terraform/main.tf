102ea5860586ea59c63adc53b4a0d71ccdf2f8c3
Brock Beatty <brockbeatty@bbeattyMAC-2.local>
2017-09-03 19:52:59 -0400

    adding work

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,19 @@
 resource "aws_instance" "example" {
   ami = "ami-a4c7edb2"
   instance_type = "t2.micro"
+  key_name = "tfKeyPair.pem"
+ 
+
+  user_data = <<-EOF
+              #!/bin/bash
+	      yum install busybox
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+
+
+  tags {
+    Name = "terraform-example"
+  }
 }
